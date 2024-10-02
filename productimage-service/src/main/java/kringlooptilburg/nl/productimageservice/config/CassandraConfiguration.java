dcd0419d969eecfd918e6f93f8a2279ee010d5ab
Abe Vriens <abe.vriens@egardia.com>
2024-10-02 10:22:44 +0200

    Connect to ScyllaDB GCloud instance

--- productimage-service/src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
+++ productimage-service/src/main/java/kringlooptilburg/nl/productimageservice/config/CassandraConfiguration.java
@@ -1,5 +1,7 @@
 package kringlooptilburg.nl.productimageservice.config;
 
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
@@ -8,18 +10,27 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @EnableCassandraRepositories
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
 
+    @Value("${spring.cassandra.contact-points}")
+    private String contactPoints;
+    @Value("${spring.cassandra.port}")
+    private int port;
+    @Value("${spring.cassandra.keyspace-name}")
+    private String keySpace;
+
     @Override
+    @NonNull
     public String getContactPoints() {
-        return "127.0.0.1";
+        return contactPoints;
     }
 
     @Override
+    @NonNull
     protected String getKeyspaceName() {
-        return "productimages";
+        return keySpace;
     }
 
     @Override
     protected int getPort() {
-        return 9042;
+        return port;
     }
 }
